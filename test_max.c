43e8caccfd70e87e12bbb11d0015b743b9ef3588
aneena0101 <85095161+aneena0101@users.noreply.github.com>
2021-06-20 19:43:41 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,7 +2,15 @@
 #include <assert.h>
 
 
-int max(int [], int);
+int max(int y[], int m)
+{ int max=y[0];
+    for(int i=0;i<m;++i)
+    { if(y[i]>max) max=y[i];
+     else continue;}
+ return max;
+}
+      
+  
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
