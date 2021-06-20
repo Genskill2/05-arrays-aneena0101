61b3e2d245d20eef4c0767b72c48d88163526872
aneena0101 <85095161+aneena0101@users.noreply.github.com>
2021-06-20 22:43:13 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -2,7 +2,39 @@
 #include <assert.h>
 
 
-int mode(int [], int);
+int mode(int num[], int k1)
+{
+	int mod,nos,d;
+	mod=nos=d=0;
+	int f[k1],er[k1];
+	int k=k1;
+	if(k!=1)
+	{
+		for(int r=0;r<k;++r)
+		{	d=num[0];
+				for(int e=r+1;e<k;++e)
+				{   if(d==num[e]) {nos++; 
+				num[e]=num[e+1];
+				k-=1;
+				}
+				er[r]=nos;
+				f[r]=num[r];
+				}
+				} 
+				for(int w=0;w<k1;++w)
+				{ mod=er[0]; d=0;
+					if(mod<er[w]) { mod=er[w]; d=w;}
+					}
+					return f[d];
+					}
+	else {d=num[0];
+		return d;
+		}
+	
+			
+					}
+					
+		
 
 int main(void) {
   int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
@@ -11,4 +43,5 @@ int main(void) {
   int y[] = {5};
   assert (mode(y, 1) == 5);
   printf("Mode: passed\n");
+  
   }
